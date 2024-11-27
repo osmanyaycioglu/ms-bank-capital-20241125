f0ab825a9ed2ae4137062cfd7a0d7595e642758f
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-11-27 17:53:29 +0300

    1

--- node-configuration/src/main/java/org/training/capital/microservice/configuration/NodeConfigurationApplication.java
+++ node-configuration/src/main/java/org/training/capital/microservice/configuration/NodeConfigurationApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableConfigServer
-@EnableScheduling
+//@EnableScheduling
 @SpringBootApplication
 public class NodeConfigurationApplication {
 
